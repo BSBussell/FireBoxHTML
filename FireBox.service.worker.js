b9e79f31416f33fcb8a49d47a3b8ce06c199ad7b
Bee Bussell <BSBussell@users.noreply.github.com>
2022-11-30 12:56:03 -0500

    Update to most recent build

--- FireBox.service.worker.js
+++ FireBox.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1669780734|4904240588";
+const CACHE_VERSION = "1669830889|23684915279";
 const CACHE_PREFIX = "FireBox-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "FireBox.offline.html";
